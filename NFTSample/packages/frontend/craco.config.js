5646027de4cd89281f6a0d002b76277989d4404a
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-07 22:23:30 +0900

    cracoの設定ファイルを追記する。

---
+++ NFTSample/packages/frontend/craco.config.js
@@ -0,0 +1,16 @@
+/**
+ * cracoの設定ファイル
+ */
+
+ module.exports = {
+    webpack: {
+      configure: (webpackConfig) => {
+        const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+          ({ constructor }) => constructor && constructor.name === "ModuleScopePlugin"
+        );
+
+        webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+        return webpackConfig;
+      },
+    },
+  };
