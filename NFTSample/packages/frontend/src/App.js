a669e4127e4ac42097d5ccd5ab43bc10116f802a
mashharuki <avp.104.106.107@ae.auone-net.jp>
2021-08-07 22:30:15 +0900

    土台部分を実装する。

--- NFTSample/packages/frontend/src/App.js
+++ NFTSample/packages/frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * メインコンポーネントファイル
+ */
 
-function App() {
+import './App.css';
+import { ethers } from "ethers";
+import {abi,bytecode} from "../../contracts/artifacts/contracts/NFT.sol/NFT.json";
+
+// プロバイダー変数を定義する。(Metamaskをインジェクションする。)
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+// NFTのdeploy時のアドレスを指定する。
+let address = "";
+
+/**
+ * App関数を定義する。(本体)
+ */
+function App () {
+  // JSXを返す。
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>動作</p>
+      <button id="test" onClick={buttonDeploy}>NFT deploy</button><br/>
+      <button id="test1" onClick={buttonGetName}>NFT get name</button><br/>
+      <button id="test2" onClick={buttonMint}>NFT mint</button><br/>
+      <button id="test3" onClick={buttonSupply}>NFT totalSupply</button>
     </div>
   );
 }
 
+// 外部にコンポーネントを公開する。
 export default App;
--- NFTSample/packages/frontend/src/index.js
+++ NFTSample/packages/frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Appコンポーネントをレンダリングする。
 ReactDOM.render(
   <React.StrictMode>
     <App />
